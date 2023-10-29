d94f0ee4f16a14c0f7f3fe5f1af55b3acc71464a
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-10-29 20:35:17 +0530

    Add Cube with Different colors and add some animation to rotate and this is demo and it will work

---
+++ js/script.js
@@ -0,0 +1,67 @@
+/* global THREE */
+// Scene, Mesh, Camera, Renderer, Clock, Animation, Resize
+
+// Scene
+const scene = new THREE.Scene();
+
+// Mesh
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = [
+  new THREE.MeshBasicMaterial({ color: 0xff5733 }),
+  new THREE.MeshBasicMaterial({ color: 0x33ff57 }),
+  new THREE.MeshBasicMaterial({ color: 0x3357ff }),
+  new THREE.MeshBasicMaterial({ color: 0xffc300 }),
+  new THREE.MeshBasicMaterial({ color: 0x8000ff }),
+  new THREE.MeshBasicMaterial({ color: 0x00ffcc }),
+];
+
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// Camera
+const aspect = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+const camera = new THREE.PerspectiveCamera(75, aspect.width / aspect.height);
+camera.position.z = 3;
+scene.add(camera);
+
+// Renderer
+const canvas = document.getElementById('canvas');
+const renderer = new THREE.WebGLRenderer({ canvas });
+
+renderer.setSize(aspect.width, aspect.height);
+
+// Clock
+const clock = new THREE.Clock();
+
+// Animation
+const animate = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  mesh.rotation.x = elapsedTime;
+  mesh.rotation.y = elapsedTime;
+  mesh.rotation.z = elapsedTime;
+
+  mesh.position.x = Math.cos(elapsedTime);
+  mesh.position.y = Math.sin(elapsedTime);
+
+  renderer.render(scene, camera);
+
+  window.requestAnimationFrame(animate);
+};
+
+animate();
+
+// Resize
+window.addEventListener('resize', () => {
+  aspect.width = window.innerWidth;
+  aspect.height = window.innerHeight;
+
+  camera.aspect = aspect.width / aspect.height;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(aspect.width, aspect.height);
+});
